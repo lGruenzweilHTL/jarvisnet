a522bf80e88563149f0aa9b291140342692f6c35
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2025-12-28 13:36:12 +0100

    Chat history and tool calls

--- model/Preset.py
+++ model/Preset.py
@@ -1,31 +1,40 @@
 ﻿from typing import Optional
 from model.request.Tool import Tool
 import ollama
-from ollama import GenerateResponse
+from ollama import Message
 
 class Preset:
     name: str
     model: str
-    system: Optional[str] = None
     tools: list[Tool] = []
     stream: bool = False
     think: bool = False
 
+    history: list[Message] = []
+
     def __init__(self, name: str, model: str, tools: Optional[list[Tool]] = None, system: Optional[str] = None, stream: bool = False, think: bool = False):
         self.name = name
         self.model = model
-        self.system = system
         self.tools = tools or []
         self.stream = stream
         self.think = think
 
-    def prompt(self, prompt, images) -> GenerateResponse:
-        return ollama.generate(
+        if system:
+            self.history.append(Message(role="system", content=system))
+
+    def prompt(self, prompt, images = None) -> str:
+        self.history.append(Message(role="user", content=prompt, images=images))
+        res = ollama.chat(
             model=self.model,
-            prompt=prompt,
-            #tools=[tool.to_schema() for tool in self.tools],
-            images=images,
-            system=self.system,
-            stream=self.stream,
+            messages=self.history,
+            tools=[tool.to_schema() for tool in self.tools],
+            stream=False, # TODO: streaming support
             think=self.think
         )
+        self.history.append(res.message)
+
+        if res.message.role == "tool":
+            # TODO: handle tool calls
+            pass
+
+        return res.message
