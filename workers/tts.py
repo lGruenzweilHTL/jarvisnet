d4fbc0393accfa36d58491e34f80f47e2c0a47e0
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2026-01-16 20:19:32 +0100

    stt and tts workers (not finished)

---
+++ workers/tts.py
@@ -0,0 +1,44 @@
+import base64
+import time
+from os.path import basename
+from fastapi import FastAPI
+from piper import PiperVoice
+
+MODEL_PATH = "models/tts/de_DE-karlsson-low.onnx"
+voice = PiperVoice.load(MODEL_PATH)
+
+app = FastAPI()
+
+@app.post("/infer")
+async def infer(data: dict):
+    start = time.perf_counter()
+
+    req_id = data["request_id"]
+    text = data["input"]["text"]
+    audio, info = synthesize(text)
+
+    end = time.perf_counter()
+    latency_ms = int((end - start) * 1000)
+    result = {
+        "request_id": req_id,
+        "output": {
+            "data_base64": audio.decode("utf-8"),
+            "sample_rate": info.sample_rate,
+            "channels": info.num_speakers,
+            "encoding": "pcm_s16le"
+        },
+        "usage": {
+            "latency_ms": latency_ms,
+            "model": basename(MODEL_PATH),
+            "version": info.piper_version,
+        },
+        "error": None
+    }
+    return result
+
+
+def synthesize(text: str):
+    global voice
+    audio = voice.synthesize(text)
+    raw_bytes = b"".join([chunk.audio_int16_bytes for chunk in audio])
+    return base64.b64encode(raw_bytes), voice.config
