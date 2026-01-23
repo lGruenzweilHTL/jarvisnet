bbc56e1b754cbe7506759d1a6a6fb5c59529f1a5
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2026-01-23 23:12:08 +0100

    Moved logging to serilog

--- AssistantCore/AssistantCore/Program.cs
+++ AssistantCore/AssistantCore/Program.cs
@@ -3,69 +3,79 @@ using AssistantCore.Tools;
 using AssistantCore.Voice;
 using AssistantCore.Workers;
 using AssistantCore.Chat;
-using AssistantCore.Logging;
 using AssistantCore.Middleware;
 using AssistantCore.Workers.LoadBalancing;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext() // captures scoped properties
+    .Enrich.WithProperty("Service", "AssistantCore")
+    // File sink as compact JSON (easy to parse)
+    .WriteTo.File(new CompactJsonFormatter(), path: "logs/assistantcore-.json",
+        rollingInterval: RollingInterval.Day, retainedFileCountLimit: 14)
+    // Seq sink for searchable dashboard (docker run --name seq -d -e ACCEPT_EULA=Y -p 5341:80 datalust/seq:latest)
+    .WriteTo.Seq("http://localhost:5341", apiKey: null, restrictedToMinimumLevel: LogEventLevel.Debug)
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Configure logging: console (default) and a simple file logger provider
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-var logDir = builder.Configuration.GetValue<string>("LogDirectory") ?? "logs";
-var logPath = Path.Combine(logDir, $"assistantcore-{DateTime.UtcNow:yyyyMMdd-HHmmss}.log");
-var fileProvider = new FileLoggerProvider(logPath);
-builder.Services.AddSingleton<ILoggerProvider>(fileProvider);
-
-builder.Services.AddControllers();
-
-builder.Services.AddSingleton(provider =>
+try
 {
-    var logger = provider.GetService<ILogger<ToolCollector>>();
-    var tc = new ToolCollector(logger!);
-    tc.SetAssemblies([Assembly.GetExecutingAssembly()]);
-    return tc;
-});
-builder.Services.AddSingleton<HttpClient>();
-builder.Services.AddSingleton(provider => ChatManager.Create(TimeSpan.FromMinutes(30)));
-builder.Services.AddSingleton<SatelliteManager>();
-builder.Services.AddSingleton<WorkerRegistry>();
-builder.Services.AddSingleton<ILoadBalancer, RoundRobinLoadBalancer>();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
 
-builder.Services.AddSingleton<ISttWorkerClient, HttpSttWorkerClient>();
-builder.Services.AddSingleton<IRoutingWorkerClient, HttpRoutingWorkerClient>();
-builder.Services.AddSingleton<ILlmWorkerClient, HttpLlmWorkerClient>();
-builder.Services.AddSingleton<ITtsWorkerClient, HttpTtsWorkerClient>();
+    builder.Services.AddControllers();
 
-var app = builder.Build();
+    builder.Services.AddSingleton(provider =>
+    {
+        var logger = provider.GetService<ILogger<ToolCollector>>();
+        var tc = new ToolCollector(logger!);
+        tc.SetAssemblies([Assembly.GetExecutingAssembly()]);
+        return tc;
+    });
+    builder.Services.AddSingleton<HttpClient>();
+    builder.Services.AddSingleton(provider => ChatManager.Create(TimeSpan.FromMinutes(30)));
+    builder.Services.AddSingleton<SatelliteManager>();
+    builder.Services.AddSingleton<WorkerRegistry>();
+    builder.Services.AddSingleton<ILoadBalancer, RoundRobinLoadBalancer>();
 
-// Register middleware for exception logging and request logging
-app.UseMiddleware<ExceptionLoggingMiddleware>();
-app.UseMiddleware<RequestLoggingMiddleware>();
+    builder.Services.AddSingleton<ISttWorkerClient, HttpSttWorkerClient>();
+    builder.Services.AddSingleton<IRoutingWorkerClient, HttpRoutingWorkerClient>();
+    builder.Services.AddSingleton<ILlmWorkerClient, HttpLlmWorkerClient>();
+    builder.Services.AddSingleton<ITtsWorkerClient, HttpTtsWorkerClient>();
 
-// Register system prompts from configuration using the host logger
-var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
-var promptsSection = builder.Configuration.GetSection("LLMConfig");
-LlmInfo.ParseConfig(promptsSection, logger);
+    var app = builder.Build();
 
-// Configure WebSockets
-app.UseWebSockets(new WebSocketOptions
+    // Register middleware for exception logging and request logging
+    app.UseMiddleware<ExceptionLoggingMiddleware>();
+    app.UseMiddleware<RequestLoggingMiddleware>();
+
+    // Register system prompts from configuration using the host logger
+    var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
+    var promptsSection = builder.Configuration.GetSection("LLMConfig");
+    LlmInfo.ParseConfig(promptsSection, logger);
+
+    // Configure WebSockets
+    app.UseWebSockets(new WebSocketOptions
+    {
+        KeepAliveInterval = TimeSpan.FromSeconds(30)
+    });
+    app.MapControllers();
+
+    // Ensure we cancel any active voice pipelines when host is shutting down
+    app.Lifetime.ApplicationStopping.Register(() =>
+    {
+        var mgr = app.Services.GetRequiredService<SatelliteManager>();
+        var startupLogger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
+        startupLogger.LogInformation("Application stopping: cancelling active satellite pipelines");
+        mgr.CancelAllPipelines();
+    });
+    
+    app.Run();
+}
+finally
 {
-    KeepAliveInterval = TimeSpan.FromSeconds(30)
-});
-app.MapControllers();
-
-// Ensure we cancel any active voice pipelines when host is shutting down
-app.Lifetime.ApplicationStopping.Register(() =>
-{
-    var mgr = app.Services.GetRequiredService<SatelliteManager>();
-    var startupLogger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
-    startupLogger.LogInformation("Application stopping: cancelling active satellite pipelines");
-    mgr.CancelAllPipelines();
-});
-
-// Https redirection is not supported for WebSocket connections
-//app.UseHttpsRedirection();
-
-
-app.Run();
+    Log.CloseAndFlush();
+}
